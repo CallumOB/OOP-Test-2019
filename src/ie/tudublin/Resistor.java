0f3a5eb0f17086d9d382e91c8d6004fa3f2a40d1
CallumOB <callumobrien2004@gmail.com>
2023-04-23 15:51:42 +0100

    Code working now.

--- src/ie/tudublin/Resistor.java
+++ src/ie/tudublin/Resistor.java
@@ -1,5 +1,7 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
+
 public class Resistor {
     int resistance;
     UI ui;
@@ -12,9 +14,40 @@ public class Resistor {
         this.resistance = resistance;
         int hundreds = resistance / 100;
         int tens = (resistance - (hundreds * 100)) / 10;
-        int ones = resistance - ((hundreds * 100) + (tens + 10));
-        o = ui.findColour(ones);
-        t = ui.findColour(tens);
+        int ones = resistance - ((hundreds * 100) + (tens * 10));
         h = ui.findColour(hundreds);
+        t = ui.findColour(tens);
+        o = ui.findColour(ones);
+    }
+
+    public void render(float x, float y) {
+        ui.pushMatrix();
+        ui.translate(x, y);
+        ui.stroke(255);
+        ui.line(-100, 0, -50, 0);
+        ui.line(-50, 0, -50, -50);
+        ui.line(-50, -50, 50, -50);
+        
+        ui.line(50, -50, 50, 0);
+        ui.line(50, 0, 100, 0);
+        ui.line(50, 0, 50, 50);
+        ui.line(50, 50, -50, 50);
+        ui.line(-50, 50, -50, 0);
+
+        // Draw the color bars
+        ui.noStroke();
+        ui.fill(h.r, h.g, h.b);
+        ui.rect(-40, -49, 10, 99);
+        
+        ui.fill(t.r, t.g, t.b);
+        ui.rect(-20, -49, 10, 99);
+        
+        ui.fill(o.r, o.g, o.b);
+        ui.rect(0, -49, 10, 99);
+        ui.fill(255);
+        ui.textAlign(PApplet.CENTER, PApplet.CENTER);
+        ui.textSize(30);
+        ui.text(resistance, 200, 0);
+        ui.popMatrix();
     }
 }
--- src/ie/tudublin/UI.java
+++ src/ie/tudublin/UI.java
@@ -11,26 +11,14 @@ public class UI extends PApplet {
 	ArrayList<Colour> colours = new ArrayList<Colour>();
 	ArrayList<Resistor> resistors = new ArrayList<Resistor>();
 
-	public void separate(int value) {
-		int hundreds = (value / 100);
-		int tens = (value - (hundreds * 100)) / 10;
-		int ones = value - ((hundreds * 100) + (tens * 10));
-		print(hundreds + ",");
-		print(tens + ",");
-		println(ones);
-	}
-
 	public void settings() {
 		size(500, 800);
 
-		separate(381);
-		separate(1);
-		separate(92);
-
-		loadResistors();
 		loadColours();
+		loadResistors();
+		printResistors();
 	}
-
+	
 	public void setup() {
 
 	}
@@ -40,7 +28,8 @@ public class UI extends PApplet {
 		stroke(255);
 
 		for (int i = 0; i < resistors.size(); i++) {
-			
+			float y = map(i, 0, resistors.size(), 100, height - 100);
+			resistors.get(i).render(width / 2, y);
 		}
 	}
 
@@ -52,7 +41,7 @@ public class UI extends PApplet {
 	}
 
 	public void loadColours() {
-		Table t = loadTable("colours.csv");
+		Table t = loadTable("colours.csv", "header");
 		for (TableRow tr : t.rows()) {
 			colours.add(new Colour(tr));
 		}
